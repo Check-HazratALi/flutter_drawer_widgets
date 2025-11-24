c113cfc442b8ea9e092cb2b60f7339d55482a645
Check-HazratAli <checkhazratali@gmail.com>
2025-11-24 11:27:02 +0600

    sidebar_with_animation

---
+++ lib/sidebar_with_animation/arc_sidebar.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:rxdart/rxdart.dart';
+
+import 'components/components.dart';
+
+class ArcSideBar extends StatefulWidget {
+  final List<SideBarItem> body;
+  final Color background;
+  final Color sideBarIconColor;
+  final Widget? header;
+  final Widget? footer;
+  final Curve? curve;
+
+  const ArcSideBar({
+    super.key,
+    required this.body,
+    required this.background,
+    this.header = const SizedBox(),
+    this.footer = const SizedBox(),
+    this.sideBarIconColor = Colors.black,
+    this.curve = Curves.linear,
+  });
+
+  @override
+  SidebarState createState() => SidebarState();
+}
+
+class SidebarState extends State<ArcSideBar> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late StreamController<bool> isSidebarOpenedStramController;
+  late Stream<bool> isSidebarOpenedStream;
+  late StreamSink<bool> isSidebarOpenedSink;
+  bool isSidebarOpened = true;
+  final _animationDuration = const Duration(milliseconds: 300);
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this, duration: _animationDuration);
+    isSidebarOpenedStramController = PublishSubject<bool>();
+    isSidebarOpenedStream = isSidebarOpenedStramController.stream;
+    isSidebarOpenedSink = isSidebarOpenedStramController.sink;
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    isSidebarOpenedStramController.close();
+    isSidebarOpenedSink.close();
+    super.dispose();
+  }
+
+  void onIconPressed() {
+    final animationStatus = _animationController.status;
+    final isAnimationCompleted = animationStatus == AnimationStatus.completed;
+
+    if (isAnimationCompleted) {
+      isSidebarOpenedSink.add(false);
+      _animationController.reverse();
+    } else {
+      isSidebarOpenedSink.add(true);
+      _animationController.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final screenWidth = size.width;
+    final screenHeight = size.height;
+    return StreamBuilder(
+      initialData: false,
+      stream: isSidebarOpenedStream,
+      builder: (context, AsyncSnapshot<bool?> isSiderBarOpenedAsync) {
+        late final bool? value;
+
+        if (isSiderBarOpenedAsync.data != null) {
+          value = isSiderBarOpenedAsync.data;
+        }
+
+        return AnimatedPositioned(
+          duration: _animationDuration,
+          top: 0,
+          bottom: 0,
+          left: value! ? 0 : -screenWidth - 20,
+          right: value ? 0 : screenWidth - 47,
+          child: Row(
+            children: [
+              Expanded(
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 200),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: screenHeight,
+                  curve: widget.curve!,
+                  decoration: BoxDecoration(color: widget.background),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        widget.header ?? SizedBox(),
+                        ...widget.body.map((item) {
+                          return SideBarItem(
+                            icon: item.icon,
+                            title: item.title,
+                            onTap: () {
+                              onIconPressed();
+                              if (item.onTap != null) {
+                                item.onTap!();
+                              }
+                            },
+                          );
+                        }),
+                        widget.footer ?? SizedBox()
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: const Alignment(0, -0.9),
+                child: GestureDetector(
+                  onTap: onIconPressed,
+                  child: ClipPath(
+                    clipper: CustomMenuclipper(),
+                    child: Container(
+                      width: 35,
+                      height: 110,
+                      color: widget.background,
+                      alignment: Alignment.centerLeft,
+                      child: AnimatedIcon(
+                        progress: _animationController.view,
+                        icon: AnimatedIcons.menu_close,
+                        color: widget.sideBarIconColor,
+                        size: 27,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/sidebar_with_animation/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_drawer/sidebar_with_animation/arc_sidebar.dart';
+
+import 'components/sidebar_item.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(title: 'Material App', home: HomeScreen(), debugShowCheckedModeBanner: false,);
+//   }
+// }
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _currentScreen = [
+    Container(color: Colors.red, width: 500, height: 1000),
+    Container(color: Colors.purpleAccent, width: 500, height: 1000),
+    Container(color: Colors.orange, width: 500, height: 1000),
+    Container(color: Colors.greenAccent, width: 500, height: 1000),
+  ];
+  void onIconPressed(int index) async {
+    if (_currentIndex == index) return; //* Prevent reconstruction of the current screen
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          _currentScreen[_currentIndex],
+          ArcSideBar(
+            background: Colors.white,
+            header: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 50),
+                  Icon(
+                    Icons.flutter_dash,
+                    size: 100,
+                    color: Colors.blueAccent,
+                  ),
+                ],
+              ),
+            ),
+            body: [
+              SideBarItem(
+                icon: Icons.play_arrow_outlined,
+                title: 'Exhibition Screen',
+                onTap: () => onIconPressed(0),
+              ),
+              SideBarItem(
+                title: 'Popular Screen',
+                icon: Icons.movie_outlined,
+                onTap: () => onIconPressed(1),
+              ),
+              SideBarItem(
+                title: 'Briefly Screen',
+                icon: Icons.card_giftcard,
+                onTap: () => onIconPressed(2),
+              ),
+            ],
+            footer: Column(
+              children: [
+                ListTile(
+                  title: Text("Dark Mode", style: TextStyle(fontWeight: FontWeight.w300, fontSize: 17)),
+                  leading: Icon(Icons.lightbulb_outline, size: 30),
+                  trailing: Switch.adaptive(
+                    value: false,
+                    onChanged: (value) {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
