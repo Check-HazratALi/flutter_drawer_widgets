da3322f402b8a04d1a96864345f729eb443cde36
Check-HazratAli <checkhazratali@gmail.com>
2025-11-24 11:21:15 +0600

    animated_corner_stack

---
+++ lib/animated_corner_stack/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_drawer/animated_corner_stack/animated_stack.dart';
+import 'package:flutter_drawer/animated_corner_stack/helpers/icon_tile.dart';
+import 'package:flutter_drawer/animated_corner_stack/helpers/item_placeholder.dart';
+
+// void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated Stack Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      home: AnimatedStack(
+        // Button Properties
+        buttonIcon: Icons.add,
+        fabIconColor: Colors.white,
+        // Animation Properties
+        animateButton: true,
+        slideAnimationDuration: Duration(milliseconds: 600),
+        buttonAnimationDuration: Duration(milliseconds: 100),
+        // Theme Properties
+        backgroundColor: Color(0xff321B4A),
+        fabBackgroundColor: Color(0xffEB456F),
+        // Content Widgets
+        foregroundWidget: _getForegroundWidget(),
+        columnWidget: _getColumnWidget(),
+        bottomWidget: _getBottomWidget(),
+        // Controllers
+        enableClickToDismiss: true,
+        preventForegroundInteractions: true,
+        onForegroundCallback: () => print('on Close Callback'),
+      ),
+    );
+  }
+
+  Widget _getForegroundWidget() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xff56377C),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 30,
+            offset: Offset(4, 4),
+          ),
+        ],
+        borderRadius: BorderRadius.all(
+          Radius.circular(40),
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ItemPlaceholder();
+        },
+      ),
+    );
+  }
+
+  Widget _getColumnWidget() {
+    return Column(
+      children: <Widget>[
+        IconTile(
+          width: 100,
+          height: 60,
+          iconData: Icons.share,
+        ),
+        SizedBox(height: 20),
+        IconTile(
+          width: 60,
+          height: 60,
+          iconData: Icons.image,
+        ),
+        SizedBox(height: 20),
+        IconTile(
+          width: 60,
+          height: 60,
+          iconData: Icons.camera_alt,
+        ),
+      ],
+    );
+  }
+
+  Widget _getBottomWidget() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xff645478),
+        borderRadius: BorderRadius.all(
+          Radius.circular(50),
+        ),
+      ),
+      width: 260,
+      height: 50,
+    );
+  }
+}
